2276f639387575fafcd87c6dd0d2c9b5856de28e
kenwan <kenwan.cheung@gmail.com>
2018-09-10 20:22:26 -0500

    cpu shell script

---
+++ test/pbscript_notes_run_cpu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=20
+#PBS -l mem=64Gb
+#PBS -j oe
+
+module load singularity tensorflow/tensorflow:1.10.0-devel-py3
+$SINGULARITY_EXEC $CONTAINER_PATH python3 /gpfs/data/ildproject-share/capstone/src/othersamples/seq2seq/seq2seq_notes.py
+
+exit
