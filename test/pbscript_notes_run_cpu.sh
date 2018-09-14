9e21c547fcbca74c9708b7ac0b96879c00477029
kenwan <kenwan.cheung@gmail.com>
2018-09-13 19:36:43 -0500

    trying old setup

--- test/pbscript_notes_run_cpu.sh
+++ test/pbscript_notes_run_cpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#PBS -l nodes=4:ppn=16
-#PBS -l mem=128Gb
+#PBS -l nodes=1:ppn=28
+#PBS -l mem=64Gb
 #PBS -j oe
 
-module load singularity tensorflow/tensorflow:1.4.0-rc0-gpu-py3
+module load singularity tensorflow/tensorflow:1.10.0-devel-py3
 $SINGULARITY_EXEC $CONTAINER_PATH python3 /gpfs/data/ildproject-share/capstone/src/othersamples/seq2seq/seq2seq_notes.py
 
 exit
