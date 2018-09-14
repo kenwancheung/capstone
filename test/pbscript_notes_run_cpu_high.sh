5c9afefd39eebaa05cab6124a1aba6c0ad2715a8
kenwan <kenwan.cheung@gmail.com>
2018-09-13 20:01:27 -0500

    updated params for run

--- test/pbscript_notes_run_cpu_high.sh
+++ test/pbscript_notes_run_cpu_high.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#PBS -l nodes=1:ppn=28 walltime=15:00:00 -q high
+#PBS -l nodes=1:ppn=28 -q high
 #PBS -l mem=128Gb
+#PBS -l walltime=15:00:00
 #PBS -j oe
 
 module load singularity tensorflow/tensorflow:1.4.0-rc0-gpu-py3
