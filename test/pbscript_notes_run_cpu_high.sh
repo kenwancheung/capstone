6a91c628cc3f14f45d26a466448276b357d365b3
kenwan <kenwan.cheung@gmail.com>
2018-09-15 13:05:25 -0500

    revised requirements for ppn

--- test/pbscript_notes_run_cpu_high.sh
+++ test/pbscript_notes_run_cpu_high.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#PBS -l nodes=1:ppn=28 -q high
+#PBS -l nodes=1:ppn=16
 #PBS -l mem=128Gb
-#PBS -l walltime=15:00:00
+#PBS -l walltime=24:00:00
 #PBS -j oe
 
 module load singularity tensorflow/tensorflow:1.4.0-rc0-gpu-py3
