b361a88fbcff5ffa2bb1fe29ad0e3dc857a260fd
kenwan <kenwan.cheung@gmail.com>
2018-09-15 13:15:46 -0500

    trying 1.10

--- test/pbscript_notes_run_cpu_high.sh
+++ test/pbscript_notes_run_cpu_high.sh
@@ -5,7 +5,7 @@
 #PBS -l walltime=24:00:00
 #PBS -j oe
 
-module load singularity tensorflow/tensorflow:1.4.0-rc0-gpu-py3
+module load singularity tensorflow/tensorflow:1.10.0-devel-py3
 $SINGULARITY_EXEC $CONTAINER_PATH python3 /gpfs/data/ildproject-share/capstone/src/othersamples/seq2seq/seq2seq_notes.py
 
 exit
